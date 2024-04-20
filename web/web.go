553fada9277ba39afd7bf13f622fbee22934d7ff
y9NBA <asarkrylov76212@gmail.com>
2024-04-20 14:50:37 +0300

    The conclusion is made more succinctly and pleasantly

--- web/web.go
+++ web/web.go
@@ -9,8 +9,8 @@ import (
 	"strconv"
 )
 
-func serveHandler(w http.ResponseWriter, r *http.Request) {
-	file, err := os.ReadFile("./web/index.html")
+func render(w http.ResponseWriter, r *http.Request, filePath string) {
+	file, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("GG")
 		return
@@ -18,25 +18,31 @@ func serveHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", file)
 }
 
+func serveHandler(w http.ResponseWriter, r *http.Request) {
+	render(w, r, "./web/index.html")
+}
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	render(w, r, "./web/result.html")
+
 	fs := logic.FibonacciService{}
 
 	number, err := strconv.Atoi(r.FormValue("numberValue"))
 
 	if err != nil || number < 0 {
-		fmt.Fprintf(w, "Введённое число некорректно!!!")
+		fmt.Fprintf(w, "<h2>Введённое число некорректно!!!<h2/>")
 		return
 	}
-
-	fmt.Fprintf(w, "Введённое число: %d\n", number)
+	fmt.Fprintf(w, "<a>Введённое число: %d\n</a><br>", number)
 
 	if fs.IsFibonacci(number) {
 		prev, next := fs.GetAdjacentFibonacci(number)
-		fmt.Fprintf(w, "Предыдущее число Фибоначчи: %d\n", prev)
-		fmt.Fprintf(w, "Следующее число Фибоначчи: %d\n", next)
+		fmt.Fprintf(w, "<a>Предыдущее число Фибоначчи: %d</a><br>", prev)
+		fmt.Fprintf(w, "<a>Следующее число Фибоначчи: %d</a><br>", next)
 	} else {
 		closest := fs.GetNearestFibonacci(number)
-		fmt.Fprintf(w, "Ближайшее число Фибоначчи: %d\n", closest)
+		fmt.Fprintf(w, "<a>Ближайшее число Фибоначчи: %d</a><br>", closest)
 	}
 }
 
