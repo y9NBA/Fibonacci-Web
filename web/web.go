b2abf083a288b7f848eeefdede14e05f8670e903
y9NBA <asarkrylov76212@gmail.com>
2024-04-22 17:15:36 +0300

    ÐAdded a word transition to return to the beginning

--- web/web.go
+++ web/web.go
@@ -31,19 +31,25 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	number, err := strconv.Atoi(r.FormValue("numberValue"))
 
 	if err != nil || number < 0 {
-		fmt.Fprintf(w, "<h2>Введённое число некорректно!!!<h2/>")
-		return
-	}
-	fmt.Fprintf(w, "<a>Введённое число: %d\n</a><br>", number)
 
-	if fs.IsFibonacci(number) {
-		prev, next := fs.GetAdjacentFibonacci(number)
-		fmt.Fprintf(w, "<a>Предыдущее число Фибоначчи: %d</a><br>", prev)
-		fmt.Fprintf(w, "<a>Следующее число Фибоначчи: %d</a><br>", next)
+		fmt.Fprintf(w, "<h2>Введённое число некорректно!!!</h2>")
+
 	} else {
-		closest := fs.GetNearestFibonacci(number)
-		fmt.Fprintf(w, "<a>Ближайшее число Фибоначчи: %d</a><br>", closest)
+
+		fmt.Fprintf(w, "<a>Введённое число: %d\n</a>", number)
+
+		if fs.IsFibonacci(number) {
+			prev, next := fs.GetAdjacentFibonacci(number)
+			fmt.Fprintf(w, "<a>Предыдущее число Фибоначчи: %d</a>", prev)
+			fmt.Fprintf(w, "<a>Следующее число Фибоначчи: %d</a>", next)
+		} else {
+			closest := fs.GetNearestFibonacci(number)
+			fmt.Fprintf(w, "<a>Ближайшее число Фибоначчи: %d</a>", closest)
+		}
+
 	}
+
+	fmt.Fprintf(w, "<a href=\"/\">Вернуться</a>")
 }
 
 func StartServer() {
